973f64f5e1fe47e18daa5d6b57ca52ac4380c3f2
alvarofae <105504676+alvarofae@users.noreply.github.com>
2024-10-25 08:32:39 -0300

    Update js informacoesGlobais.js

--- graficos/js informacoesGlobais.js
+++ graficos/js informacoesGlobais.js
@@ -4,4 +4,16 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
     console.log(dados);
+    const paragrafo = documento.creatElement('p')
+    paragrafo.classList.add('grafico-container__texto')
+
+    paragrafo.innerHTML = `Você sabia que o mundo /
+    tem <span>${dados.total_pessoas_mundo}</span> /
+     de pessoas e que aproximadamente /
+     <span>${dados.total_pessoas_conectadas}</span> estão /
+     conectadas em alguma rede social e passam /
+     em média <span>${dados.tempo_medio}</span> /
+     horas conectadas.`
+    console.log(paragrafo)
+
 }
